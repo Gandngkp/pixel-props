dfa9037b7d971ea799d4805920ac3118f9128583
Gndngkp <gandang.kharisma123@gmail.com>
2020-08-06 11:48:52 +0700

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,13 @@
+##########################################################################################
+# M""""""""M                   dP          #
+# Mmmm  mmmM                   88          #
+# MMMM  MMMM .d8888b. .d8888b. 88 .d8888b. #
+# MMMM  MMMM 88ooood8 Y8ooooo. 88 88'  `88 #
+# MMMM  MMMM 88.  ...       88 88 88.  .88 #
+# MMMM  MMMM `88888P' `88888P' dP `88888P8 #
+# MMMMMMMMMM                               # Pixel 4 XL (Coral) - August 2020
+##########################################################################################
+
+#Print welcome message
+ui_print "- Installing, Pixel 4 XL (Coral) Prop - August 2020"
+ui_print "- By Gan, Telegram: @gndngkp"
